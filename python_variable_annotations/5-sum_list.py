48e5e3044a230eb5d7b10f3dc2fa804c4ac6b1c2
Emma <furlan.emmanuelle@neuf.fr>
2025-07-29 18:08:58 +0200

    sum of a list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Modules that define function which takes a list of floats as argument.
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Return the sum of as a float."""
+    return sum(input_list)
