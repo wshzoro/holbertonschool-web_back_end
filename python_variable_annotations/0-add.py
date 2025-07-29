316f85e53d0885eb11de20517a1d845f73b6e70d
Emma <furlan.emmanuelle@neuf.fr>
2025-07-29 16:55:27 +0200

    correct docstring

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,7 +2,8 @@
 """Module that provides a function to sum two floats.
 """
 
+
 def add(a: float, b: float) -> float:
-    """Returns the sum.
+    """Returns the sum of two float numbers.
     """
     return a + b
