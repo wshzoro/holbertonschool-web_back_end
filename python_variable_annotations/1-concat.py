aefe954a912fbccfd56e79541efbd46d0ec83786
Emma <furlan.emmanuelle@neuf.fr>
2025-07-29 17:06:33 +0200

    correct docstring

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""Type-annotated function concat.
+"""
 
 def concat(str1: str, str2: str) -> str:
-    """concats two strings"""
-    return str1 + str2
+    """Return concatenation of two strings.
+    """
 
+    return str1 + str2
