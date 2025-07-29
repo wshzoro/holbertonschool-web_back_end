bc73478943d8ea22703c3db3961db2c400ec0647
Emma <furlan.emmanuelle@neuf.fr>
2025-07-30 00:04:44 +0200

    execute files

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-"""Modules that define function which takes a list of floats and ints.
-"""
+"""This module defines a function that returns the sum of a mixed list of integers and floats."""
+
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: List[int, float]) -> float:
-    """Return the sum of as a float.
-    """
-    return sum(mxd_list)
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
+    """Return the sum of a list of integers and floats as a float."""
+    return float(sum(mxd_list))
--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-"""Modules that define function the return a tuple of
-a string and a square of a number.
-"""
+"""This module defines a function that returns a tuple of a string and the square of a number."""
+
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Return the sum of as a float.
-    """
-    return sum(k, float(v ** 2))
+    """Return a tuple with the string and the square of the number as a float."""
+    return (k, float(v ** 2))
