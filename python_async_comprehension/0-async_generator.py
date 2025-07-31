587f494260d4464fe91e26af8ebbdc70eab11118
Emma <furlan.emmanuelle@neuf.fr>
2025-07-31 10:56:30 +0200

    correct pycodstyle

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
-Async Generator:
-- Yields 10 random floats between 0 and 10
-- Waits 1 second between each yield
+Module that defines an asynchronous generator.
+The generator yields 10 random floats between 0 and 10,
+with a 1-second pause between each yield.
 """
 import asyncio
 import random
@@ -11,8 +11,8 @@ from typing import AsyncGenerator
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """
-    Asynchronous generator that yields 10 random numbers between 0 and 10,
-    one every second.
+    Asynchronous generator that yields 10 random numbers between 0 and 10.
+    Waits 1 second between each yield.
     """
     for _ in range(10):
         await asyncio.sleep(1)
