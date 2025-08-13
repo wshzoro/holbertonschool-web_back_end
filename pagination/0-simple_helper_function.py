4cc3cfde430ad48c730b3e4db334b7b16f684ff4
Emma <furlan.emmanuelle@neuf.fr>
2025-08-13 15:02:19 +0200

    first pagination

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python3
-"""
-function named index_range that takes
-two int args page and page_size
-"""
+"""function named index_range that takes
+    two integer arguments page and page_size"""
+
 
 def index_range(page: int, page_size: int) -> tuple:
-    """
-    return a tuple of size two containing
-    a start index and an end index
-    """
-
+    """return a tuple of size two containing a start index and an end index"""
     return ((page - 1) * page_size, page * page_size)
-
