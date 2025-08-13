d28c4ca0ce7218c0999ea0ec5f14e774173c3a09
Emma <furlan.emmanuelle@neuf.fr>
2025-08-13 14:59:59 +0200

    first pagination

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python3
 """
-This module provides a helper function for pagination.
-It calculates the start and end indexes for retrieving a 
-specific page of results from a dataset.
+function named index_range that takes
+two int args page and page_size
 """
 
 from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-Calculate the start and end index for a given page and page size.
-
-Args:
-    page(int): the page number (1-indexed, must be >= 1)
-    page_size (int): number of items per page (must be > 0)
-    
-Returns:
-    Tuple[int, int]: A tuple containig (start_index end_index)
+    return a tuple of size two containing
+    a start index and an end index
     """
 
     start_index = (page - 1) * page_size
