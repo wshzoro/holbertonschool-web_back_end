8ae8ae03db12cb80b828e51dbc45b1f0bcab1625
Emma <furlan.emmanuelle@neuf.fr>
2025-08-21 13:36:09 +0200

    implement class

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,54 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    // Types checks
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students) || !students.every((s) => typeof s === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
+    // Assignations
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getters
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  // Setters
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
+  }
+
+  set students(value) {
+    if (!Array.isArray(value) || !value.every((s) => typeof s === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = value;
+  }
+}
