495d4610d298b2df1b99be8110f0a244e2213c2d
Emma <furlan.emmanuelle@neuf.fr>
2025-08-21 14:15:10 +0200

    implementation bulding

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,25 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+
+    this._sqft = sqft;
+
+    if (
+      this.constructor !== Building
+      && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage
+    ) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  // Getter
+  get sqtf() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error(`${this.constructor.name} must override evacuationWarningMessage`);
+  }
+}
