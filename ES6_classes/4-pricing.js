a6d26600717b468a01380e469e1d7a53724e9110
Emma <furlan.emmanuelle@neuf.fr>
2025-08-21 14:01:45 +0200

    import currency

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,47 @@
+import Currency from './3-currency';
+
+export default class Princing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+
+    this._amount = amount;
+    this._currency = currency;
+  }
+  // Getters
+
+  get amount() {
+    return this.amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  // Setters
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = value;
+  }
+
+  // Méthode statique
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Amount and conversionRate must be numbers');
+    }
+    return amount * conversionRate;
+  }
+}
