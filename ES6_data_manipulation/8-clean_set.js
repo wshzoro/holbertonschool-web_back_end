fde0bd193571152e6f4ed5866081f743d78f0355
Emma <furlan.emmanuelle@neuf.fr>
2025-08-25 14:52:10 +0200

    correct

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   const result = [];
   for (const value of set) {
-    if (value.statsWith(startString) && startString !== '') {
+    if (value.startsWith(startString) && startString !== '') {
       result.push(value.slice(startString.length));
     }
   }
