30c56f66b4803f5b0069f9d5e150bcd40d89e704
Emma <furlan.emmanuelle@neuf.fr>
2025-08-25 14:55:06 +0200

    fix code

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
   const result = [];
   for (const value of set) {
-    if (value.startsWith(startString) && startString !== '') {
-      result.push(value.slice(startString.length));
+    if (startString === '' || value.startsWith(startString)) {
+      result.push(startString === '' ? value : value.slice(startString.length));
     }
   }
   return result.join('-');
