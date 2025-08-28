57846fc2704f06c2db4ffe52a250f8743533f175
Emma <furlan.emmanuelle@neuf.fr>
2025-08-28 11:35:22 +0200

    upload pp

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const { body } = results[0];
+      const { firstName, lastName } = results[1];
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
