b001e2ffb22d4b6b0c4598d52c26737aba58bf56
Emma <furlan.emmanuelle@neuf.fr>
2025-08-29 22:39:37 +0200

    final user

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,13 +2,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUp = signUpUser(firstName, lastName);
-  const photo = uploadPhoto(fileName);
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)
+  ];
 
-  return Promise.allSettled([signUp, photo]).then((results) => results.map(
-    (result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    }),
-  ));
+  return Promise.allSettled(promises)
+    .then((results) => {
+      return results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
+      }));
+    });
 }
