95f9483224308f79aa8a3f73e3c556638f9c2fdd
Emma <furlan.emmanuelle@neuf.fr>
2025-09-01 13:08:42 +0200

    async

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+          reject(new Error('Cannot load the database'));
+          return;
+        }
+        const lines = data.split('\n').filter(line =>line.trim() !== '');
+        const students = lines.slice(1);
+
+        console.log(`Number of students: ${students.length}`);
+
+        const fields = {};
+        students.forEach(line => {
+          const parts = line.split(',');
+          const firstname = parts[0].trim();
+          const field = parts[parts.length -1].trim();
+
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(firstname);          
+        });
+        const fieldOrder = Object.keys(fields);
+        fieldOrder.forEach(field => {
+          console.log(
+            `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
+          );
+        });
+        resolve();
+    });
+  });
+}
+module.exports = countStudents;
