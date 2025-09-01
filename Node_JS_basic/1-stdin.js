b4641cdd5ffc63a15dc89802f3a5f441de23edb4
Emma <furlan.emmanuelle@neuf.fr>
2025-09-01 11:24:15 +0200

    fix bug

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,13 +3,9 @@ console.log("Welcome to Holberton School, what is your name?");
 process.stdin.resume();
 process.stdin.setEncoding("utf-8");
 
-process.stdin.once("data", (data) => {
+process.stdin.on("data", (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
-
-  if (process.stdin.isTTY) {
-    process.exit(0);
-  }
 });
 
 process.stdin.on("end", () => {
