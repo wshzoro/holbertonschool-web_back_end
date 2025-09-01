815a37a25dc3562c08d9d2b52abd526262169f9e
Emma <furlan.emmanuelle@neuf.fr>
2025-09-01 11:13:12 +0200

    add if

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,15 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+console.log("Welcome to Holberton School, what is your name?");
 
 process.stdin.on("data", (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
-  process.exit(0);
+
+  if (process.stdin.isTTY) {
+    process.exit(0);
+  }
 });
 
-process.on("exit", () => {
+process.stdin.on("end", () => {
   console.log("This important software is now closing");
 });
+process.stdin.resume();
