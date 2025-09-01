dfbfa9397d76e9a9462fc3f9623b786741495b91
Emma <furlan.emmanuelle@neuf.fr>
2025-09-01 11:14:44 +0200

    add if

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 console.log("Welcome to Holberton School, what is your name?");
 
-process.stdin.on("data", (data) => {
+process.stdin.once("data", (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
 
