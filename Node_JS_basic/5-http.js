308bf95770b7b305d15e5bb988df1b49f7a2c20b
Emma <furlan.emmanuelle@neuf.fr>
2025-09-01 14:03:15 +0200

    http more complex

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const database = process.argv[2];
+
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    let output = '';
+    const originalConsoleLog = console.log;
+    console.log = (msg) => { output += `${msg}\n`; };
+
+    countStudents(database)
+      .then(() => {
+        console.log = originalConsoleLog;
+        res.end(`This is the list of our students\n${output}`);
+      })
+      .catch((err) => {
+        res.end(err.toString());
+      });
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
