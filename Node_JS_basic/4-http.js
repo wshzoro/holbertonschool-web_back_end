f57ff0fdb4f467041472287760a9f1727a235952
Emma <furlan.emmanuelle@neuf.fr>
2025-09-01 13:21:07 +0200

    create a server

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!')
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245);
