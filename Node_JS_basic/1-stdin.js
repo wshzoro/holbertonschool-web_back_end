b07dafa0eafe0b2c5f4b83d58083bc89880f7880
Emma <furlan.emmanuelle@neuf.fr>
2025-09-01 11:02:55 +0200

    fix bug

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,11 @@
-console.log("Welcome to Holberton School, what is your name?");
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on("data", (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
+  process.exit(0);
 });
 
-process.stdin.on("end", () => {
+process.on("exit", () => {
   console.log("This important software is now closing");
 });
-
-process.stdin.resume();
