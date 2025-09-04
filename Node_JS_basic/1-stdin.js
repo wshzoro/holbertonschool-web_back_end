feb589dae37cff0fc2de6d4b51d891d2788a49ca
Emma <furlan.emmanuelle@neuf.fr>
2025-09-04 14:04:59 +0200

    debug file

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,12 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
-  const name = data.trim();
+  const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
