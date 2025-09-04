3b06df9a0aa6b3c1af105286cadfcb109a7c200c
Emma <furlan.emmanuelle@neuf.fr>
2025-09-04 14:13:43 +0200

    fix bug

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,37 +1,38 @@
-import express from 'express';
-import countStudents from './3-read_file_async';
+const express = require('express');
+const countStudents = require('./3-read_file_async');
 
-const database = process.argv[2];
+const databasePath = process.argv[2];
+
 const app = express();
 
 app.get('/', (req, res) => {
-  res.type('text/plain').send('Hello Holberton School!');
+  res.set('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  const text = 'This is the list of our students\n';
+
+  const originalLog = console.log;
+
   let output = '';
-  const originalConsoleLog = console.log;
 
-  console.log = (msg) => { output += `${msg}\n`; };
+  console.log = (message) => { output += `${message}\n`; };
 
-  countStudents(database)
+  countStudents(databasePath)
+  // restore original log and show answer
     .then(() => {
-      console.log = originalConsoleLog;
-      res.type('text/plain').status(200)
-        .send(`This is the list of our students\n${output}`);
+      console.log = originalLog;
+      res.send(text + output.trim());
     })
     .catch(() => {
-      console.log = originalConsoleLog;
-      res.type('text/plain').status(200)
-        .send('This is the list of our students\nCannot load the database');
+      // restore original log and error message
+      console.log = originalLog;
+      res.send('Cannot load the database');
     });
 });
 
-app.use((req, res) => {
-  res.status(404).type('text/plain').send('Not Found');
-});
-
-// Écoute sur le port 1245
 app.listen(1245);
 
-export default app;
+module.exports = app;
