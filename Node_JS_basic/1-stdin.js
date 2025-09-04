48ba6b5044d89273259c250127f9bc135fdd4cb2
Emma <furlan.emmanuelle@neuf.fr>
2025-09-04 14:01:11 +0200

    correct file

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (Input) => {
-  process.stdout.write(`Your name is: ${Input}`);
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+  console.log(`Your name is: ${name}`);
 });
