8cb6b6fbf1cc5045e3b16cbd99d7e6eb6802be8e
Emma <furlan.emmanuelle@neuf.fr>
2025-09-04 14:12:40 +0200

    fix bug

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,33 +1,37 @@
 import express from 'express';
-import countStudents from './3-read_file_async.js';
+import countStudents from './3-read_file_async';
 
 const database = process.argv[2];
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+  res.type('text/plain').send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
   let output = '';
   const originalConsoleLog = console.log;
+
   console.log = (msg) => { output += `${msg}\n`; };
 
   countStudents(database)
     .then(() => {
       console.log = originalConsoleLog;
-      res.status(200).send(`This is the list of our students\n${output}`);
+      res.type('text/plain').status(200)
+        .send(`This is the list of our students\n${output}`);
     })
     .catch(() => {
       console.log = originalConsoleLog;
-      res.status(200).send('This is the list of our students\nCannot load the database');
+      res.type('text/plain').status(200)
+        .send('This is the list of our students\nCannot load the database');
     });
 });
 
 app.use((req, res) => {
-  res.status(404).send('Not Found');
+  res.status(404).type('text/plain').send('Not Found');
 });
 
+// Écoute sur le port 1245
 app.listen(1245);
 
 export default app;
