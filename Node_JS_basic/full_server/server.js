5b90530442868adc7192d83ee23ca5b2aa93814b
Emma <furlan.emmanuelle@neuf.fr>
2025-09-04 10:33:13 +0200

    principal file

---
+++ Node_JS_basic/full_server/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes/index';
+
+const app = express();
+
+app.use(routes);
+
+app.listen(1245, () => {
+  console.log('Server running on port 1245');
+});
+
+export default app;
